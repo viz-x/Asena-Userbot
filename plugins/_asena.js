19baf5355311f3e8a4524979735d2a0ea6f72aca
Pʀᴀɴᴀᴠ Sᴇʀ <80111660+Viz-Zer@users.noreply.github.com>
2022-02-09 22:29:32 +0530

    Asᴇɴᴀ

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -51,7 +51,7 @@ Asena.addCommand(
   { pattern: "help ?(.*)", fromMe: true, dontAddCommandList: true },
   async (message, match) => {
     let CMD_HELP = `╭────────────────╮
-    ᴡʜᴀᴛsᴀᴘᴘ-ʙᴏᴛ
+     ᴄᴏᴍᴍᴀɴᴅs
 ╰────────────────╯
 
 ╭────────────────
