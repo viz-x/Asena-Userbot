47c69ff336cc9f969e9dec08cc957ab7a81e393a
Pʀᴀɴᴀᴠ Sᴇʀ <80111660+Viz-Zer@users.noreply.github.com>
2022-03-28 23:20:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ module.exports = {
     WARN_COUNT: process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
     WARN_MSG: process.env.WARN_MSG === undefined ? "Ok bie" : process.env.WARN_MSG,
     ANTIJID: process.env.ANTIJID === undefined ? '' : process.env.ANTIJID,
-    STICKER_PACKNAME: process.env.STICKER_PACKNAME === undefined ? '🥰,lyfe00011' : process.env.STICKER_PACKNAME,
+    STICKER_PACKNAME: process.env.STICKER_PACKNAME === undefined ? '💞,WhatsAsena' : process.env.STICKER_PACKNAME,
     BRAINSHOP: process.env.BRAINSHOP === undefined ? "159501,6pq8dPiYt7PdqHz3" : process.env.BRAINSHOP,
     DIS_BOT: process.env.DISABLE_BOT === undefined ? "null" : process.env.DISABLE_BOT
 };
